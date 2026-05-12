74b0a3e6bc87306f1c7e3a2ffeb04b71476692b3
Tika Meutia Lubis <111536774+tikameutia@users.noreply.github.com>
2026-05-12 10:40:51 +0200

    Edited the context, added a waiting message for ux

--- src/app.js
+++ src/app.js
@@ -14,11 +14,15 @@ function generatePoem(event) {
 
   let apiKey = "0050t012172bb4a92f6b53332o6cb5f4";
   let prompt = `Please make a short and funny Sinterklaas poem in Dutch, the present is ${present.value}`;
-  let context = `You are a witty, smart and funny Sinterklaas poem maker. The poem that you created will be beautifully rhymed and short. Please provide your poem with a proper <br /> element use so we can read the rhyme better. The Dutch translation word of the ${present.value} to be written with <strong> element. End the poem with a signature 'Liefs, Sint en Piet'.`;
+  let context = `You are a witty, smart and funny Sinterklaas poem maker. The poem that you created will be beautifully rhymed and short. Please provide your poem with a proper <br /> element use so we can read the rhyme better. The Dutch translation of ${present.value} to be written with <strong> element. End the poem with a signature 'Liefs, Sint en Piet'.`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  poem.innerHTML = "Generating a Sinterklaas poem for you.. please wait";
 
   axios.get(apiUrl).then(showPoem);
 }
+
+let poem = document.querySelector("#poem");
 
 let form = document.querySelector("form");
 form.addEventListener("submit", generatePoem);

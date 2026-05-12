4e986319662016c68324891f445a06a400ab7d1d
Tika Meutia Lubis <111536774+tikameutia@users.noreply.github.com>
2026-05-12 09:58:55 +0200

    Added Typewriter js plugin

--- src/app.js
+++ src/app.js
@@ -3,6 +3,13 @@ function showPoem(event) {
   let poem = document.querySelector("#poem");
   poem.innerHTML =
     "Sint weet uit het verleden, Jij bent altijd snel tevreden. Daarom dit pakje, klein maar fijn. Daar zul je vast wel blij mee zijn.";
+
+  new Typewriter("#poem", {
+    strings: poem.innerHTML,
+    autoStart: true,
+    cursor: null,
+    delay: 10,
+  });
 }
 
 let form = document.querySelector("form");
